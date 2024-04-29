2aed5fb255241ba0c7b21e62fbf82aa1e7ce601f
huss2012 <huss24940@gmail.com>
2024-04-29 22:53:50 +0300

    Phase 1 has been solved.

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,11 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string === "string") {
+    return string.split("").reverse().join("");
+  } else {
+    throw new TypeError("Invalid argument! Must be a string");
+  }
 };
+
+//-----------Local test area-----------
+//console.log(reverseString(10))
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,19 @@
 // Your code here
+const chai = require("chai");
+const  expect  = chai.expect;
+const reverseString = require("../problems/reverse-string.js");
+
+describe("reverseString", function () {
+
+    it("must reverse the input string", function () {
+        let input = reverseString("fun");
+
+        expect(input).to.equal("nuf");
+    })
+
+    it("Should throws TypeError when the passed argument is not a string.", function () {
+        expect(() => reverseString(10)).to.throw(TypeError, "Invalid argument! Must be a string");
+
+
+    })
+})
