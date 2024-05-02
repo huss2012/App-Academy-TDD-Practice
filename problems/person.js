fa666348c8103c190073c984363bdd6d0e6ecb7a
huss2012 <huss24940@gmail.com>
2024-05-02 15:56:20 +0300

    Phase04: Person class test specs  solved.

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,52 @@
 class Person {
   // Your code here
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Hi, ${this.name}. Nice to meet you.`;
+  }
+
+  visit(person) {
+    return `${this.name} visited ${person.name}.`;
+  }
+  switchVisit = (otherPerson) => otherPerson.visit(this);
+
+  update(obj) {
+    if (typeof obj !== "object" || obj === null || Array.isArray(obj) || !Object.keys(obj).includes("name") || !Object.keys(obj).includes("age")) {
+      throw new TypeError("The passed argument is not a valid object.");
+    };
+    this.name = obj.name;
+    this.age = obj.age;
+  }
 }
 
+//----------Local Test Area-----------------
+const person1 = new Person("Hussain", 30);
+const person2 = new Person("Ali", 30);
+//Update case:
+person1.update({ name: "DDDD", age: 24 })
+console.log(person1);
+//passing only name:
+// person1.update({ name: "DDDD"})
+// console.log(person1);
+//passing only age:
+// person1.update({ age: 24 })
+// console.log(person1);
+//Passing empty object:
+// person1.update({ })
+// console.log(person1);
+//Passing string:
+// person1.update("Hussain");
+// console.log(person1);
+//Passing function:
+// person1.update(() => { })
+// console.log(person1);
+//Passing array:
+// person1.update([]);
+// console.log(person1);
+console.log(person1.switchVisit(person2));
+console.log(person1.visit(person2));
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,84 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const Person = require("../problems/person.js");
+
+let person1;
+let person2;
+let typeErrorMessage;
+beforeEach(() => {
+    person1 = new Person("Hussain", 30);
+    person2 = new Person("Ali", 40);
+    typeErrorMessage = "The passed argument is not a valid object.";
+});
+describe("Person Class.", function () {
+    describe("Constructor.", function () {
+
+        it("Should create an instance of Person with name and age properties.", function () {
+            expect(person1.name).to.equal("Hussain");
+            expect(person1.age).to.equal(30);
+        });
+        it("Should the properties name and age exist in the instance.", function () {
+            expect(person1.name).to.exist;
+            expect(person1.age).to.exist;
+        });
+
+    });
+
+    describe("sayHello instance Method.", function () {
+        it("Should return a string of the person's name and a greeting message.", function () {
+
+            expect(person1.sayHello()).to.equal('Hi, Hussain. Nice to meet you.');
+        });
+    });
+
+    describe("visit instance method.", function () {
+
+        it("Should return a string that stating this instance visited the passed in person instance.", function () {
+            expect(person1.visit(person2)).to.equal('Hussain visited Ali.');
+        });
+    });
+
+    describe("switchVisit instance method.", function () {
+        it("Should invoke visit method and pass this person instance in.", function () {
+            expect(person1.switchVisit(person2)).to.equal("Ali visited Hussain.");
+        });
+    });
+
+    describe("update instance method.", function () {
+
+        context("If the incoming argument is not valid object.", function () {
+
+            it("Should throw a TypeError if the incoming argument is not an object.", function () {
+
+                expect(() => person1.update(([]))).to.Throw(TypeError, typeErrorMessage);
+                expect(() => person1.update("Hello!")).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update([1, 2, 3])).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update("")).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update({ })).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update(() => { })).to.throw(TypeError, typeErrorMessage);
+
+            });
+
+            it("Should throw a TypeError if the incoming argument is object but do not has name and age peoperties.", function () {
+
+                expect(() => person1.update({})).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update({ name: "AAAA" })).to.throw(TypeError, typeErrorMessage);
+                expect(() => person1.update({age: 25})).to.throw(TypeError, typeErrorMessage);
+            });
+        });
+
+        context("If the incoming argument is a valid object.", function () {
+
+            it("Should update the properties of the instance with the incoming object.", function () {
+                let obj = { name: "DDDD", age: 24 };
+                person1.update(obj);
+
+                expect(person1.name).to.equal("DDDD");
+                expect(person1.age).to.equal(24);
+            })
+        });
+    })
+
+
+})
